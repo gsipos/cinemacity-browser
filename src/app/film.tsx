acaa9d3d15c073631c8f07b2c5890afe6a815aa7
Gergely Sipos <gergely.sipos@aliz.ai>
2021-12-22 23:42:28 +0100

    changed selection indicator on films

--- src/app/film.tsx
+++ src/app/film.tsx
@@ -1,29 +1,21 @@
 import { Favorite, FavoriteBorder } from '@mui/icons-material'
-import {
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Checkbox,
-  Typography,
-} from '@mui/material'
+import { Button, Card, CardActionArea, CardActions, CardHeader, CardMedia, Checkbox, Typography } from '@mui/material'
 import React from 'react'
 import { Film } from './data/data'
 
 export const FilmDisplay = ({ film, active, toggle }: { film: Film; active: boolean; toggle: () => void }) => (
   <Card elevation={active ? 24 : 2} onClick={toggle}>
     <CardActionArea>
-      <CardContent>
-        <Typography gutterBottom variant="body1" component="h6">
-          {film.name}
-        </Typography>
-      </CardContent>
+      <CardHeader
+        title={
+          <Typography gutterBottom variant="body1" component="h6">
+            {film.name}
+          </Typography>
+        }
+        action={<Checkbox checked={active} icon={<FavoriteBorder />} checkedIcon={<Favorite color="primary" />} />}
+      ></CardHeader>
       <CardMedia component="img" image={film.posterLink} />
       <CardActions>
-        <Checkbox checked={active} icon={<FavoriteBorder />} checkedIcon={<Favorite color="primary" />} />
         <Button variant="text" size="small" color="primary" href={film.link}>
           Cinema city
         </Button>
